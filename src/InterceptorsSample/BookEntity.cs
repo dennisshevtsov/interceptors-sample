46afa3ad86b4635cd39557de91a2951df9d08332
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2023-12-25 08:36:17 -0800

    add repos

---
+++ src/InterceptorsSample/BookEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace InterceptorsSample;
+
+public sealed class BookEntity(Guid id, string title)
+{
+  public Guid Id { get; set; } = id;
+
+  public string Title { get; set; } = title;
+}
---
+++ src/InterceptorsSample/BookRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InterceptorsSample
+{
+  public sealed class BookRepository(DbContext dbContext)
+  {
+    private readonly DbContext _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+    public Task<BookEntity?> GetBookAsync(Guid id, CancellationToken cancellationToken) =>
+      _dbContext.Set<BookEntity>()
+                .AsNoTracking()
+                .Where(entity => entity.Id == id)
+                .FirstOrDefaultAsync(cancellationToken);
+  }
+}
